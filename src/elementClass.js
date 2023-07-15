0ba188e287756bf5265d669259f39b4bca518576
BlueAlien449 <99145495+BlueAlien449@users.noreply.github.com>
2023-07-14 22:18:00 -0600

    Create virtual DOM

---
+++ src/elementClass.js
@@ -0,0 +1,46 @@
+export default class Element {
+    constructor(elementType) {
+      this.elementType = elementType;
+      this.attributes = {};
+      this.children = [];
+    }
+  
+    addAttributes(attributes) {
+      for (const k of Object.keys(attributes)) {
+        this.attributes[k] = attributes[k];
+      }
+      return this;
+    }
+
+    addChild(childElement) {
+        this.innerText = undefined;
+        this.children.push(childElement);
+
+        return this;
+    }
+
+    setInnerText(string) {
+        this.children = [];
+        this.innerText = string;
+
+        return this;
+    }
+
+    build() {
+        let DOMelement = document.createElement(this.elementType);
+
+        for (const k of Object.keys(this.attributes)) {
+            DOMelement.setAttribute(k, this.attributes[k]);
+        }
+
+        if (this.innerText === undefined) {
+            for (const child of this.children) {
+                DOMelement.appendChild(child.build());
+            }
+        } else {
+            let DOMinnerText = document.createTextNode(this.innerText);
+            DOMelement.appendChild(DOMinnerText);
+        }
+        return DOMelement;
+    }
+}
