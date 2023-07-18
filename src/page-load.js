4c1b7f3e0fbb59cc6f799f720ed9a160ad059b6e
BlueAlien449 <99145495+BlueAlien449@users.noreply.github.com>
2023-07-18 13:45:55 -0600

    Create nav, header, loadpage and footer function and export them

---
+++ src/page-load.js
@@ -0,0 +1,48 @@
+import Element from "./elementClass";
+import createHome from "./home";
+import { contentDiv } from "./home";
+
+export function loadPage(){
+    createNav()
+    createHeader() 
+    createHome()
+    createFooter()
+}
+
+export function createNav() {
+    const navBar = new Element('div')
+    .addAttributes({ id: 'navbar'})
+    .addChild(new Element('ul').addAttributes({ id: 'nav-list' })
+    .addChild(new Element('li').addAttributes({ id: 'home', class: 'nav-item' }).setInnerText('Home'))
+    .addChild(new Element('li').addAttributes({ id: 'menu', class: 'nav-item' }).setInnerText('Menu'))
+    .addChild(new Element('li').addAttributes({ id: 'contact', class: 'nav-item' }).setInnerText('Contact')))
+
+    contentDiv.appendChild(navBar.build());
+}
+
+export function createHeader() {
+    const header = new Element('div')
+    .addAttributes({ id: 'header' })
+    .addChild(new Element('h1').addAttributes({ id: 'header-title' }).setInnerText('Mocha Haven'))
+
+    contentDiv.appendChild(header.build());
+}
+
+export function createFooter() {
+    const footer = new Element('div')
+    .addAttributes({ id: 'footer' })
+    .addChild(new Element('div').addAttributes({ id: 'footer-title' })
+    .addChild(new Element('h3').setInnerText('Mocha Haven')))
+    .addChild(new Element('div').addAttributes({ id: 'footer-nav' })    
+    .addChild(new Element('ul').addAttributes({ id: 'nav-list' })
+    .addChild(new Element('li').addAttributes({ id: 'home', class: 'nav-item' }).setInnerText('Home'))
+    .addChild(new Element('li').addAttributes({ id: 'menu', class: 'nav-item' }).setInnerText('Menu'))
+    .addChild(new Element('li').addAttributes({ id: 'contact', class: 'nav-item' }).setInnerText('Contact'))))
+    .addChild(new Element('div').addAttributes({ id: 'footer-text-container' })
+    .addChild(new Element('p').addAttributes({ id: 'footer-text' })
+    .setInnerText('Indulge in moments of pure delight at Mocha Haven, where exceptional coffee meets warm hospitality')))
+
+
+    contentDiv.appendChild(footer.build());
+
+}
