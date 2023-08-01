7511563b6081443692403c3136fad1091f5622d4
BlueAlien449 <99145495+BlueAlien449@users.noreply.github.com>
2023-08-01 12:45:11 -0600

    Create contentLoading function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-import {loadPage} from "./page-load";
+import { loadPage } from "./page-load";
+import createHome from "./home";
+import createMenu from "./menu";
+import createContact from "./contact";
 import './css/style.css'
 
-loadPage()
+window.addEventListener('DOMContentLoaded', loadPage);
+
+
+
+export function contentLoading(){
+    let navButtons = document.querySelectorAll('.nav-item');
+    navButtons.forEach(x => x.addEventListener('click', selectPage));
+    createHome()
+    
+    function selectPage(e){
+        let currentPage = e.target.value;
+        if (currentPage === 1) {
+            removePage()
+            createHome();
+        } else if (currentPage === 2){
+            removePage()
+            createMenu();
+        } else if (currentPage === 3){
+            removePage()
+            createContact();
+        }
+    }
+
+    function removePage(){
+        let pageContentDiv = document.getElementById('page-content')
+        pageContentDiv.removeChild(pageContentDiv.firstChild);
+    }
+}
+
+
+
+
+
 
 
